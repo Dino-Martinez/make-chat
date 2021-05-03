4221cdc676b5bdfd96d8b3c9ce32fe85f66f0bba
Dino-Martinez <Dino.Martinez.514@gmail.com>
2021-05-03 11:42:35 -0700

    integrated socket.io

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
+//Express setup
 const express = require("express");
 const app = express();
-
 const server = require("http").Server(app);
 
+// Socket setup
+const io = require("socket.io")(server);
+io.on("connection", socket => {
+  console.log("User connected");
+});
+
+// Handlebars setup
 const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
+app.use("/public", express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("index.handlebars");
